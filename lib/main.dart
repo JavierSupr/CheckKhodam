4f7497d168c63e340c0ac8e5b0433ef426d4be80
Abyanaka <ahmedberylb@gmail.com>
2024-12-04 11:43:03 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:checkkhodam/screens/landing_page.dart';
 import 'package:checkkhodam/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/explanation_page.dart'; // Mengimpor file landing_page.dart
 
 void main() {
@@ -12,7 +14,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomePage(), // Menggunakan LandingPage dari file terpisah
+       home: FutureBuilder(
+        future: _checkAgreementStatus(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // Tampilkan loading indicator sementara
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.data == true) {
+            // Jika sudah setuju, langsung ke Landing Page
+            return const LandingPage();
+          } else {
+            // Jika belum setuju, tampilkan Welcome Page
+            return const WelcomePage();
+          }
+        },
+      ),
     );
+  }
+
+  Future<bool> _checkAgreementStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    // Cek apakah sudah ada status persetujuan
+    bool? isAgreed = prefs.getBool('isAgreed');
+    return isAgreed ?? false;  // Jika null, berarti belum setuju, kembalikan false
   }
 }
